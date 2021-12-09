6446f60ac10039200d98a849f52649eba13f0591
AntaresScorpius <rohitc171@gamil.com>
2021-12-09 20:57:04 +0530

    small changes

--- app.js
+++ app.js
@@ -9,9 +9,8 @@
 //manual approach (create package.json and write the properties)
 //npm init (step by step  will ask you about version and etc)
 //npm init -y (default)
-
-
 const _ = require('lodash')
 const items = [1,[2,[3,[4]]]]
 const newItems = _.flattenDeep(items)
 console.log(newItems);
+console.log('hello world');
