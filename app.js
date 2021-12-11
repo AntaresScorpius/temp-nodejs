75f8a87258de87c9585e8bdcf0054515dc5de87d
AntaresScorpius <rohitc171@gamil.com>
2021-12-11 13:55:16 +0530

    update ignore

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
-//local
-//npm install <pkgname>
+const EventEmitter = require('events');
 
-//global
-// npm install -g <pkgname>
+const customEmitter = new EventEmitter()
+//on - listen to an event
+//emit - emit an event
 
-//package.json manifest file
-//ways to create package.json
-//manual approach (create package.json and write the properties)
-//npm init (step by step  will ask you about version and etc)
-//npm init -y (default)
-const _ = require('lodash')
-const items = [1,[2,[3,[4]]]]
-const newItems = _.flattenDeep(items)
-console.log(newItems);
-console.log('hello world');
+customEmitter.on('response', ()=> {
+    console.log(`data recieved`);
+})
+
+customEmitter.emit('response')
