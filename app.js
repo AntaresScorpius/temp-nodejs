2e62b59d321d9176c51968247e4d4c4e025bee73
AntaresScorpius <rohitc171@gamil.com>
2021-12-11 22:23:37 +0530

    events

--- app.js
+++ app.js
@@ -3,22 +3,4 @@
 
 // stream.on('data', (result) =>{
 //     console.log(result);
-// })
-function ra1(){  
-    return new Promise((resolve)=> {
-        for(let i = 0; i < 900000; i++){
-            for(let j =0; j < 10000;j++){
-                
-            }
-        }
-        resolve("wait over")
-    });
-}
-
-async function ra(){
-    
-    var i = await ra1();
-    console.log("outside");
-}
-ra();
-console.log("Heello world");
+// }) 
