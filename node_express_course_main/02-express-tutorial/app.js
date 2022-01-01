e3d325a3c01b0cb6e2b28ad21ca50bef7f27d6ff
AntaresScorpius <rohitc171@gmail.com>
2022-01-01 22:44:30 +0530

    Added morgon middleware

--- node_express_course_main/02-express-tutorial/app.js
+++ node_express_course_main/02-express-tutorial/app.js
@@ -1,5 +1,6 @@
 const { json } = require('express')
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const {products} = require('./data')
 const logger = require('./logger')
@@ -7,7 +8,8 @@ const authorize = require('./authorize')
 //req => middleware => res
 //.use is invoked for every get
 // if path is provided then applies to all paths having path as root 
-app.use([logger,authorize])  // order matters before every get()
+//app.use([logger,authorize])  // order matters before every get()
+app.use(morgan('tiny'))
 app.get('/',(req,res)=>{
     res.send('Home')
 })
@@ -15,10 +17,9 @@ app.get('/about',(req,res)=>{
     res.send('About')
 })
 app.get('/api/products',(req,res)=>{
-    
     res.send('Products')
 })
-app.get('/api/items',(req,res)=>{
+app.get('/api/items',authorize,(req,res)=>{
     console.log(req.user);
     res.send('Ittems')
 })
