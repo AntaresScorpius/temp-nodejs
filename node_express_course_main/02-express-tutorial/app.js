6622eece5448966070cc95a2cadd74b07dda8bbf
AntaresScorpius <rohitc171@gmail.com>
2022-01-02 23:56:30 +0530

    MVC

--- node_express_course_main/02-express-tutorial/app.js
+++ node_express_course_main/02-express-tutorial/app.js
@@ -1,28 +1,19 @@
 const { json } = require('express')
 const express = require('express')
-const morgan = require('morgan')
+//let {people} = require('./data')
 const app = express()
-const {products} = require('./data')
-const logger = require('./logger')
-const authorize = require('./authorize')
-//req => middleware => res
-//.use is invoked for every get
-// if path is provided then applies to all paths having path as root 
-//app.use([logger,authorize])  // order matters before every get()
-app.use(morgan('tiny'))
-app.get('/',(req,res)=>{
-    res.send('Home')
-})
-app.get('/about',(req,res)=>{
-    res.send('About')
-})
-app.get('/api/products',(req,res)=>{
-    res.send('Products')
-})
-app.get('/api/items',authorize,(req,res)=>{
-    console.log(req.user);
-    res.send('Ittems')
-})
+//static assets
+app.use(express.static('./methods-public'))
+// parse form data
+app.use(express.urlencoded({extended:false }))
+app.use(express.json())
+const people = require ('./routes/people')
+const auth = require('./routes/auth')
+
+app.use('/api/people',people)
+app.use('/login', auth)
+
+
 
 app.listen(5000, ()=> {
     console.log('server is runing om 5000');
